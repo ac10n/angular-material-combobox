d1d6ea77134972f890c922981501957debfaa461
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2020-04-05 16:53:28 +0800

    remove all existing code

--- projects/angular-material-combobox/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-material-combobox
- */
-
-export * from './lib/angular-material-combobox.service';
-export * from './lib/angular-material-combobox.component';
-export * from './lib/angular-material-combobox.module';
