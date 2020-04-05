f16a47e604de8b5ab80d63d45ed4cc13af82b37b
AlirezaHaghshenas <alirezahaghshenas@gmail.com>
2020-04-05 23:13:58 +0800

    Revert "Recreate in angular9 (#5)" (#6)
    This reverts commit 110a7e2c52f8bc07d1fb43f5d65a9a1b5be78ade.

--- projects/angular-material-combobox/src/lib/angular-material-combobox.service.spec.ts
+++ projects/angular-material-combobox/src/lib/angular-material-combobox.service.spec.ts
@@ -3,14 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { AngularMaterialComboboxService } from './angular-material-combobox.service';
 
 describe('AngularMaterialComboboxService', () => {
-  let service: AngularMaterialComboboxService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AngularMaterialComboboxService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
+    const service: AngularMaterialComboboxService = TestBed.get(AngularMaterialComboboxService);
     expect(service).toBeTruthy();
   });
 });
