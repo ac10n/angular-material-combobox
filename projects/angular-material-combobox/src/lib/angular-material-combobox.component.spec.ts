a47dc6c52db4afcdbfed7c3989e8b572ce07abe8
AlirezaHaghshenas <alirezahaghshenas@gmail.com>
2020-12-16 12:36:53 +0800

    Upgrade to 11 (#14)
    * install schematic temporarily
    
    * update to ng 11
    
    * upgrade material and other dependencies
    
    * remove temporary dependency

--- projects/angular-material-combobox/src/lib/angular-material-combobox.component.spec.ts
+++ projects/angular-material-combobox/src/lib/angular-material-combobox.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AngularMaterialComboboxComponent } from './angular-material-combobox.component';
 
@@ -6,7 +6,7 @@ describe('AngularMaterialComboboxComponent', () => {
   let component: AngularMaterialComboboxComponent;
   let fixture: ComponentFixture<AngularMaterialComboboxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AngularMaterialComboboxComponent ]
     })
